0ee4c8d15f88d37f76e1a76b92a1071fad5c25ef
ThiagoBorges81 <ultimatesportsciences@gmail.com>
2023-01-09 12:28:50 -0300

    Criado funcao de exebicao de mensagem

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def gather_data():
-    n1 = int(input("Primeiro valor: "))
-    n2 = int(input("Segundo valor: "))
-    return n1,n2
+    n1 = input("Primeiro valor: ")
+    n2 = input("Segundo valor: ")
+
+    return n1,n2, op
+
+def print_message(n1,n2):
+    print(f'Os valores{n1} e {n2} somados dão {n1+n2}')
+
+    return None
+    
 
 def main():
-    n1,n2 = gather_data()
+    n1,n2,op = gather_data()
 
-    print(n1**n2)
+    print(eval(n1+op+n2))
 
     return None
 
